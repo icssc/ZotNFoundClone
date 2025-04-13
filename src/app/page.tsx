424e8520725b7e917e137a20019dde60db782c81
Brendan Lieu <brendanlieu05@gmail.com>
2025-04-12 19:50:45 -0700

    Recreated about page + Implemented Responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Providers } from "@/components/ContextProvider";
 import ItemDisplayList from "@/components/ItemDisplayList";
 import { Skeleton } from "@/components/ui/skeleton";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 // https://stackoverflow.com/questions/77978480/nextjs-with-react-leaflet-ssr-webpack-window-not-defined-icon-not-found
 // https://nextjs.org/docs/pages/building-your-application/optimizing/lazy-loading
@@ -15,6 +16,9 @@ export default function Home() {
   return (
     <Providers>
       <div className="md:h-[85vh] w-full flex flex-col justify-center items-center p-4">
+      {/* <Link href="/about" className="bg-white">
+        Hello
+      </Link> */}
         <main className="w-full p-12 h-full flex flex-row">
           <div className="flex flex-row gap-10 h-full w-full">
             <div className="w-8/10 h-full">
