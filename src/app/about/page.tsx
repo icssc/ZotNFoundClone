7becd9b5bd5ccba9b95548342fc50eb87446f7a2
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-04-12 22:20:09 -0700

    Small changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,133 +1,127 @@
 import Image from "next/image";
 import Link from "next/link";
-// import { Button } from "@/components/ui/button";
-import ZotnFoundIG from "../../../public/zotnfound_ig.png"
-import ZotnFoundSmallLogo from "../../../public/ZotnFound_Small_Logo.png"
-import GetStartedImage from "../../../public/get_started.png"
-import NavigateAroundImage from "../../../public/navigate_around.png"
-import SearchForImage from "../../../public/search_for.png"
 
 export default function About() {
   const carousel = [
     {
       title: "Get Started - It's Simple & Easy",
-      description: "Log in with your UCI email and start listing lost & found items!",
-      image: GetStartedImage
+      description:
+        "Log in with your UCI email and start listing lost & found items!",
+      image: "/get_started.png",
+      alt: "Getting Started Image",
     },
     {
       title: "Navigate Around the Map",
-      description: "Effortlessly navigate UCI's interactive map to efficiently search for lost and found items.",
-      image: NavigateAroundImage
+      description:
+        "Effortlessly navigate UCI's interactive map to efficiently search for lost and found items.",
+      image: "/navigate_around.png",
+      alt: "Navigate Around Map Image",
     },
     {
       title: "Search for Lost & Found Items",
-      description: "Utilize the easy-to-use filter and search bar to look up specific items.",
-      image: SearchForImage
+      description:
+        "Utilize the easy-to-use filter and search bar to look up specific items.",
+      image: "/search_for.png",
+      alt: "Search for Lost & Found Items Image",
     },
-  ]
-  
+  ];
+
   return (
     <div className="text-center min-h-screen space-y-8 bg-slate-100">
       <div className="mx-4 md:mx-32 py-8 space-y-8">
-
-      
-      <div className="flex flex-row place-content-end items-center">
-        {/* <Link href="/" className="bg-gray-200 rounded-lg ">
-          <button className="p-2">Return Home</button>
-        </Link> */}
-        <Link href="/">
-          <div className="flex flex-row gap-4 items-center">
-            <h1 className="text-2xl font-bold">ZotnFound</h1>
-            <Image
-              src={ZotnFoundSmallLogo}
-              alt="zotnfound_small_logo"
-              width={50}
-              height={50}
+        <div className="flex flex-row place-content-end items-center">
+          <Link href="/">
+            <div className="flex flex-row gap-4 items-center">
+              <h1 className="text-2xl font-bold">ZotnFound</h1>
+              <Image
+                src="/ZotnFound_Small_Logo.png"
+                alt="ZotnFound Logo"
+                width={50}
+                height={50}
               />
-          </div>
-        </Link>
-      </div>
-      
-      <div className="max-w-5xl mx-auto">
-        <h1 className="text-4xl font-bold">
-          We are <span className="text-blue-500">ZotnFound</span>
-        </h1>
+            </div>
+          </Link>
+        </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
-          <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
-            <p className="text-red-400 font-bold text-3xl">88</p>
-            <p className="text-sm">Lost Items</p>
-          </div>
-          <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
-            <p className="text-green-400 font-bold text-3xl">32</p>
-            <p className="text-sm">Found Items</p>
-          </div>
-          <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
-            <p className="text-yellow-400 font-bold text-3xl">16</p>
-            <p className="text-sm">Successful Items</p>
-          </div>
-          <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
-            <p className="text-purple-400 font-bold text-3xl">705</p>
-            <p className="text-sm">Active Users</p>
+        <div className="max-w-5xl mx-auto">
+          <h1 className="text-4xl font-bold">
+            We are <span className="text-blue-500">ZotnFound</span>
+          </h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-6">
+            <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
+              <p className="text-red-400 font-bold text-3xl">88</p>
+              <p className="text-sm">Lost Items</p>
+            </div>
+            <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
+              <p className="text-green-400 font-bold text-3xl">32</p>
+              <p className="text-sm">Found Items</p>
+            </div>
+            <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
+              <p className="text-yellow-400 font-bold text-3xl">16</p>
+              <p className="text-sm">Successful Items</p>
+            </div>
+            <div className="px-10 py-8 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white text-center">
+              <p className="text-purple-400 font-bold text-3xl">705</p>
+              <p className="text-sm">Active Users</p>
+            </div>
           </div>
         </div>
-      </div>
-      
-      <div>
-  <h1 className="text-2xl font-bold text-center mb-6">Explore how ZotnFound works</h1>
 
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
-  {carousel.map((item, idx) => (
-    <div
-      key={idx}
-      className="w-full max-w-sm px-4 py-4 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white flex flex-col items-center"
-    >
-      <Image
-        src={item.image}
-        alt="zotnfound_ig"
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="w-full h-auto rounded-md"
-      />
-      <div className="text-center max-w-full mt-2">
-        <p className="text-md font-semibold">{item.title}</p>
-        <p className="text-xs mt-1">{item.description}</p>
-      </div>
-    </div>
-  ))}
-</div>
+        <div>
+          <h1 className="text-2xl font-bold text-center mb-6">
+            Explore how ZotnFound works
+          </h1>
 
-</div>
-
-
-
-
-
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-8 py-4 items-center justify-center">
-      <div className="flex-1">
-        <h1 className="text-2xl font-bold mb-2">Origin of ZotnFound</h1>
-        <p className="text-gray-700 text-sm">
-          Many people are constantly losing their belongings, whether that be their phones, keys, or water bottles. This is especially true for UCI students on the UCI subreddit, where there are countless posts being created about lost and found items. Due to this problem, we decided to take matters into our own hands and created an Instagram account to help lost items return back to their original owners. We have so far helped over 10 people and gained over 300+ followers.  
-        </p>
-      </div>
-      <div className="flex justify-center">
-        <div className="w-[400px]">
-          <Image
-            src={ZotnFoundIG}
-            alt="zotnfound_ig"
-            width={400}
-            height={400}
-            className="rounded-md"
-          />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+            {carousel.map((item, idx) => (
+              <div
+                key={idx}
+                className="w-full max-w-sm px-4 py-4 rounded-md shadow-lg transition-transform duration-300 hover:scale-105 bg-white flex flex-col items-center"
+              >
+                <Image
+                  src={item.image}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  className="w-full h-auto rounded-md"
+                  alt={item.alt}
+                />
+                <div className="text-center max-w-full mt-2">
+                  <p className="text-md font-semibold">{item.title}</p>
+                  <p className="text-xs mt-1">{item.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </div>
 
-
-
-
-
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-8 py-4 items-center justify-center">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold mb-2">Origin of ZotnFound</h1>
+            <p className="text-gray-700 text-sm">
+              Many people are constantly losing their belongings, whether that
+              be their phones, keys, or water bottles. This is especially true
+              for UCI students on the UCI subreddit, where there are countless
+              posts being created about lost and found items. Due to this
+              problem, we decided to take matters into our own hands and created
+              an Instagram account to help lost items return back to their
+              original owners. We have so far helped over 10 people and gained
+              over 300+ followers.
+            </p>
+          </div>
+          <div className="flex justify-center">
+            <div className="w-[400px]">
+              <Image
+                src="zotnfound_ig.png"
+                alt="ZotnFound Instagram"
+                width={400}
+                height={400}
+                className="rounded-md"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Providers } from "@/components/ContextProvider";
 import ItemDisplayList from "@/components/ItemDisplayList";
 import { Skeleton } from "@/components/ui/skeleton";
 import dynamic from "next/dynamic";
-import Link from "next/link";
 
 // https://stackoverflow.com/questions/77978480/nextjs-with-react-leaflet-ssr-webpack-window-not-defined-icon-not-found
 // https://nextjs.org/docs/pages/building-your-application/optimizing/lazy-loading
@@ -16,9 +15,6 @@ export default function Home() {
   return (
     <Providers>
       <div className="md:h-[85vh] w-full flex flex-col justify-center items-center p-4">
-      {/* <Link href="/about" className="bg-white">
-        Hello
-      </Link> */}
         <main className="w-full p-12 h-full flex flex-row">
           <div className="flex flex-row gap-10 h-full w-full">
             <div className="w-8/10 h-full">
