3e88a3e876f82cbd6c4231d88820d05ca0f376f4
rebecca-123 <rebecca.notw@gmail.com>
2025-04-24 21:50:00 -0700

    add error handling

--- src/server/actions/item/create/action.ts
+++ src/server/actions/item/create/action.ts
@@ -4,6 +4,14 @@ import { db } from "@/db";
 import { items, NewItem } from "@/db/schema";
 
 export async function createItem(itemData: NewItem) {
-  const [newItem] = await db.insert(items).values(itemData).returning();
-  return newItem;
+  try {
+    const [newItem] = await db.insert(items).values(itemData).returning();
+    return newItem;
+  } catch (error){
+      if (error instanceof Error) {
+        return {error: `Error adding item to database: ${error.message}`};
+      } else {
+        return {error: `Error adding item to database: ${String(error)}`};
+      }
+  }
 }
