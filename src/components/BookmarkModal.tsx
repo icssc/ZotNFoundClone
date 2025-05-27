5c6da93c7f48c1aa4c6c1173967832d43713bdcb
rebecca-123 <rebecca.notw@gmail.com>
2025-05-26 19:17:18 -0700

    remove unused imports

--- src/components/BookmarkModal.tsx
+++ src/components/BookmarkModal.tsx
@@ -5,8 +5,6 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
