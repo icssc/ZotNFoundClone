5ee3bc0af61a9ccbc4a227ec738af5ccad5bf6bc
rebecca-123 <rebecca.notw@gmail.com>
2025-05-26 19:15:59 -0700

    formatting

--- src/components/BookmarkModal.tsx
+++ src/components/BookmarkModal.tsx
@@ -1,6 +1,6 @@
-import { Bell, BookmarkIcon, Search } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { Bell, BookmarkIcon, Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import {
   Dialog,
@@ -10,16 +10,16 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 
 export function BookmarkModal() {
-    const keywords: string[] = ['test', 'iphone', 'wallet', 'keychain'];
+  const keywords: string[] = ["test", "iphone", "wallet", "keychain"];
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="hover:text-gray-300 flex items-center gap-1">
-            <BookmarkIcon className="h-4 w-4" />
-            <span>Bookmarks</span>
+          <BookmarkIcon className="h-4 w-4" />
+          <span>Bookmarks</span>
         </div>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
@@ -27,27 +27,31 @@ export function BookmarkModal() {
           <DialogTitle>Saved Searches</DialogTitle>
         </DialogHeader>
         <ScrollArea className="h-[30vh] rounded-md border p-4">
-            {keywords.length === 0 && (<p>No saved searches. Use the search bar to save search keywords and get alerts!</p>)}
-            {keywords.map((searchTerm) => (
-                <div
-                    key={searchTerm}
-                    className="flex justify-between items-center p-3"
-                >
-                    {searchTerm}
-                    <div>
-                        <Button className="mr-2"
-                        >
-                          <Bell className="w-4 h-4 mr-2 " />
-                          Remove Alerts
-                        </Button>
-                        <Button>
-                            <Search className="w-4 h-4" />
-                        </Button>
-                    </div>
-                </div>
-                  ))}
-            </ScrollArea>
+          {keywords.length === 0 && (
+            <p>
+              No saved searches. Use the search bar to save search keywords and
+              get alerts!
+            </p>
+          )}
+          {keywords.map((searchTerm) => (
+            <div
+              key={searchTerm}
+              className="flex justify-between items-center p-3"
+            >
+              {searchTerm}
+              <div>
+                <Button className="mr-2">
+                  <Bell className="w-4 h-4 mr-2 " />
+                  Remove Alerts
+                </Button>
+                <Button>
+                  <Search className="w-4 h-4" />
+                </Button>
+              </div>
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
