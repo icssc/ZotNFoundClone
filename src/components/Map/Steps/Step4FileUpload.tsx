f32d0a91810f4b324f4c3318bf5bc030f4cabcc7
rebecca-123 <rebecca.notw@gmail.com>
2025-10-25 21:14:58 -0700

    feat: file compression

--- src/components/Map/Steps/Step4FileUpload.tsx
+++ src/components/Map/Steps/Step4FileUpload.tsx
@@ -1,6 +1,7 @@
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { LocationFormData } from "@/lib/types";
+import imageCompression from "browser-image-compression";
 
 interface Step4Props {
   updateField: <K extends keyof LocationFormData>(
@@ -19,8 +20,18 @@ export function Step4FileUpload({ updateField }: Step4Props) {
         id="file"
         type="file"
         accept="image/*"
-        onChange={(e) => {
-          const file = e.target.files?.[0] || null;
+        onChange={async (e) => {
+          let file = e.target.files?.[0] || null;
+          if (file) {
+            const options = {
+              maxSizeMB: 1,
+              maxWidthOrHeight: 1920,
+              useWebWorker: true,
+              fileType: "image/webp",
+              preserveExif: false,
+            };
+            file = await imageCompression(file, options);
+          }
           updateField("file", file);
         }}
         className="py-32 bg-slate-400"
