1ea382e92efb4b2886b488275beccbec31a7af2d
rebecca-123 <rebecca.notw@gmail.com>
2025-10-25 21:27:08 -0700

    chore: format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
-      ".sst/**"
+      ".sst/**",
     ],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
